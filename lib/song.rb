8fbd22f7b885d440292916cb91818b2e99802a74
Andrew Cheon <cheonandrew113@gmail.com>
2019-07-22 09:51:41 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,18 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize(name, artist_name)
+    @name = name
+    @artist_name = artist_name
+    Song.all << self
+  end
+
+
+
+  def self.find_by_name
+    @@all.name
+  end
+
   def self.all
     @@all
   end
@@ -9,5 +21,6 @@ class Song
   def save
     self.class.all << self
   end
+  song = Song.new(name)
 
 end
